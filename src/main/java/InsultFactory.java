a9eee85568e873dfeafde893803140c1ab0ad20b
Samme Janderson <samme.san@gmail.com>
2020-12-05 12:35:37 -0300

    created procfile

---
+++ src/main/java/InsultFactory.java
@@ -0,0 +1,69 @@
+public class InsultFactory {
+
+    static String[] insults = {"“%s não vale mais nada, mais eu te chamaria Valete.”",
+
+            "“%s tu é deformado, torto, idade e sere, \n" +
+                    " Cara deformada, corpo feio, sem forma em todos os lugares; \n" +
+                    " Vicioso, ungentle, tolo, sem corte, grosseiro; \n" +
+                    " Estigmatico na tomada, pior em mente “.",
+
+            "“%s! Filho de meretrizes, vilão sem sentido!”",
+
+            "“Você %s és um notavél covarde, um infinito e sem fim mentiroso, de hora em hora um traidor, dententor de nenhuma virtude”",
+
+            "“Silêncio %s !\nMais de sua conversa poderia infectar meu cérebro.”",
+
+            "“A acidez do rosto de %s azeda uvas maduras.”",
+
+            "“Pra longe %s! Tu és veneno para o meu sangue “.",
+
+            "“%s têm uma abundante falta de inteligência.”",
+
+            "“%s, incestuoso assassino, condenado bandido",
+
+            "“%s! Covarde otimista, este preguiçoso, este pecador, esta colina enorme de carne!”",
+
+            "“%s! Fanfarrão vil e maldito furioso!",
+
+
+            "“%s! \n Dentes tivesses tu na tua cabeça quando nasceste,\n Para significar teres vindo a morder o mundo.”",
+
+            "“%s!\n Você blocos, você pedras, você pior do que coisas sem sentido!”",
+
+            "“%s é um tolo tedioso.”",
+
+            "“%s! Ó covarde infiel! Ó patife desonesto!\n Queres ficar são um homem fora do meu vício?”",
+
+            "%s és um mictório rei castelhano!”",
+
+            "“O céu sabe muito bem que %s és falso(a) como o inferno.”",
+
+            "“Fora da minha vista! fazes infectar meus olhos.”",
+
+            "“%s és um camponês pretendente! Tu és um filho da puta burro de carga de cavalo malte!”",
+
+            "“Por que,%s ? … Irá tu dizer uma mentira monstruosa, sendo apenas metade de um peixe e metade de um monstro?”",
+
+            "“%s! filho de puta-lobo!”",
+
+            "“%s não tens mais cérebro do que eu tenho em meus cotovelos.”",
+
+            "“%s! Vá pendurar-se, você impertinente zombeteiro!”",
+
+            "“Eu vou cortar sua língua %s.”\n" +
+                    " “Mas isso não importará, vou falar como muito sagacidade como tu depois.”",
+
+            "“%s, eu te daria uma surra, mas isso infectaria minhas mãos“",
+
+            "“O rosto do(a) %s não vale a pena o sol queimar“",
+
+            "“Era %s um(a) traficante de carne, um(a) tolo(a) e um covarde?“",
+
+            "“Aqui está %s tão belo(a) quanto uma rã“",
+
+
+
+};
+}
+
+
